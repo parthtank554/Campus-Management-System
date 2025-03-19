b8c9f95b93bf21ed048bab77326031dfd1123d02
Parth Tank <132151538+parthtank554@users.noreply.github.com>
2025-03-19 11:28:57 +0530

    added Session Functionality

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Campus_Management_System.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == "Admin" && password == "Admin123")
+            {
+                HttpContext.Session.SetString("username", username);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "InValid Username Or Password";
+                return View();
+            }
+        }
+        public IActionResult LogOut()
+        {
+            HttpContext.Session.Remove("username");
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
