fa80b4a2a40e789895157e6fe42681db4dc6fe09
Parth Tank <132151538+parthtank554@users.noreply.github.com>
2025-03-24 09:21:07 +0530

    New Controller Add

---
+++ Controllers/TeacherController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Campus_Management_System.Controllers
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddTeacher()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/TeacherModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Campus_Management_System.Models
+{
+    public class TeacherModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter name")]
+        public string TeacherName { get; set; }
+
+        [Required(ErrorMessage = "Please enter Subject")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Please enter Dept")]
+        public string Semester { get; set; }
+        public string Experience { get; set; }
+
+        [Required(ErrorMessage = "please enter salary")]
+        [Range(5000, 50000, ErrorMessage = "Value should be between 5k to 50k")]
+        public int Salary { get; set; }
+    }
+}
